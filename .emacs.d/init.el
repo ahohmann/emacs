3c9c82976f4e59d48b94bb6744f721efaf44e5f0
Andreas Hohmann <ahohmann@server.fake>
2014-09-27 20:19:54 -0400

    js2-mode tweaks

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -43,6 +43,8 @@
 	"~/.emacs.d/site-list/yasnippet/snippets"))
 (yas-global-mode 1)
 
+(add-to-list 'load-path "~/.emacs.d/site-lisp/js2-mode")
+(require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 ;;; markdown
@@ -55,18 +57,19 @@
 
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
  '(c-offsets-alist (quote ((innamespace . 0))))
  '(column-number-mode t)
+ '(js2-basic-offset 2)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
